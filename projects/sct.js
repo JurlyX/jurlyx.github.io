c3b7eda4a368ccab0e1a3148a658f3e04b3fbf32
JurlyX <161505209+JurlyX@users.noreply.github.com>
2025-10-22 12:23:58 -0400

    Refactor variable declarations in op function

--- projects/sct.js
+++ projects/sct.js
@@ -1,6 +1,6 @@
 function op() { 
-  var money = 82585834768932743,
-	clickGain = 523750346882402376,
-	autoGain = 905348725727584568325345,
-	interval;
+  var money = 82585834768932743;
+  var clickGain = 523750346882402376;
+  var autoGain = 905348725727584568325345;
+  updateMoney()
 }
